574ad3700faee1d13dfa8b8b81fe5fec3e57e0db
Ryan <rsbombard@gmail.com>
2017-09-23 15:31:50 -0600

    add boxofficefox console command

---
+++ app/Console/Commands/FetchBoxOfficeData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FetchBoxOfficeData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tickets:boxoffice';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch box office event listings.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $scraper = new \App\Services\TicketService();
+
+        $scraper->fetchBoxOfficeListings( 500, 10 );
+
+        $log = $scraper->getLog();
+        foreach ($log as $entry) {
+            $this->info($entry);
+        }
+    }
+}
