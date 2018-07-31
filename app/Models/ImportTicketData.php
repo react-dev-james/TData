459ba8d08ba42df9e05846405949d00f5f3d9b61
Sung-Whi Kim <sungwhiwork@gmail.com>
2018-07-31 14:42:34 +0200

    updated master_table import to use CSV from airtable w/o modification

--- app/Models/ImportTicketData.php
+++ app/Models/ImportTicketData.php
@@ -8,44 +8,16 @@ class ImportTicketData
 {
     public static function import()
     {
-
-        /* delete old lines */
-
-
-        if (($handle = fopen("test.csv", "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $num = count($data);
-                echo "<p> $num fields in line $row: <br /></p>\n";
-                $row++;
-                for ($c=0; $c < $num; $c++) {
-                    echo $data[$c] . "<br />\n";
-                }
-            }
-            fclose($handle);
-        }
-
+        // open file
+        $handle = fopen(storage_path('app/Master_Table.csv'), 'r');
 
         // set line counter
         $line_number = 1;
 
-        // open file
-        //$data = file_get_contents(storage_path('app/Master_Table.csv'), 'r');
-        $handle = fopen(storage_path('app/Master_Table.csv'), 'r');
-
-        //$lines = explode("\r", $data);
-
-       /// echo count($lines) . " Entries Found For Importing";
-
         // process it line by line
-        foreach( $line = fgetcsv($handle, 2000, ","))
+        while( ($line = fgetcsv($handle, 2000, ",")) !== false )
         {
             echo $line_number . "\n";
-
-            print_r($line);
-
-            if($line_number > 20) break;
-            /*
-            $ticketItem = [];
 
             // assign line data to array
             list(
@@ -71,7 +43,7 @@ class ImportTicketData
                 $ticketItem['sfc_roi'],
                 $ticketItem['sfc_roi_dollar'],
                 $ticketItem['sfc_cogs']
-             ) = preg_split("/[\t]/", $line);
+             ) = $line;
 
             // assign category
             $category = $ticketItem['category'];
@@ -87,13 +59,8 @@ class ImportTicketData
                 $val = trim(str_replace(['$'], '', $val));
 
                 // check for empty and invalid values adn set to 0
-                if (empty($val) || $val == '#DIV/0!' || $val == "-") {
+                if (empty($val) || $val == '#DIV/0!' || $val == "-" || strtolower($val) === 'nan') {
                     $val = 0;
-                }
-
-                // set NaN to null
-                if( strtolower($val) === 'nan') {
-                    $val = null;
                 }
 
                 return $val;
@@ -111,7 +78,7 @@ class ImportTicketData
             } else {
                 echo "|";
             }
-            */
+
             // increment line number
             $line_number++;
         }
