f8a7fa64417803ac91968e82b01094b79c2a6217
Sung-Whi Kim <sungwhiwork@gmail.com>
2018-08-27 16:46:40 +0200

    changing update stats back

--- app/Models/UpdateStats.php
+++ app/Models/UpdateStats.php
@@ -16,11 +16,17 @@ class UpdateStats
 
     public static function update()
     {
-        $listings = \App\Listing::whereNotNull('data_master_id')->with('data')->get();
+        $listings = \App\Listing::get();
 
         foreach ($listings as $listing) {
 
             /* Calculate ROI for listing */
+            if ( !isset($listing->data) || $listing->data === null ) {
+                echo '/** Data object not found in UpdateStats.php for listing id: ' . $listing->id . " **/\n";
+                Log::info('/** Data object not found in UpdateStats.php for listing id: ' . $listing->id . ' **/');
+                continue;
+            }
+
             $listing->calcRoi($listing->data);
         }
     }
